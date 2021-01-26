4137c9a7f4afc48ed0695a5c3f18467a8d4f49cd
kai <pineappleclown@gmail.com>
2021-01-26 22:21:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,35 @@
-from discord.ext import commands
+# coding:UTF-8
+import discord
 import os
-import traceback
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+from discord.ext import tasks
+from datetime import datetime, timedelta, timezone
 
+TOKEN = "ODAzNTg1MDkwMjc3NjA1Mzk3.YA_67w.sBRDwpnP__Y9CYyqigp4Cz1GAYQ"  # トークン
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+# ↓はトークンの自動取得をしようとしてできなかった残骸
+# token = os.environ['DISCORD_BOT_TOKEN']
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+client = discord.Client()
 
 
-bot.run(token)
+@client.event
+async def on_message(message):
+    # ユーザが"/date"と入力するのを検知
+    if message.content.startswith("date"):
+
+        # 送り主がBotだった場合反応したくないので
+        if client.user != message.author:
+
+            # 現在の時刻を取得して"now"変数に格納
+            JST = timezone(timedelta(hours=+9), 'JST')
+            now = datetime.now(JST).strftime('%H:%M')
+
+            # メッセージを"ms"変数に格納
+            ms = now + "に" + message.author.name + "さんがタイムカードを切りました。"
+
+            # メッセージを出力
+            await message.channel.send(ms)
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
